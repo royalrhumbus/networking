fc8b90d3eb137326de12e6f6649a2282736dbfe0
royalrhumbus <maxsorozco@gmail.com>
2017-10-30 20:14:42 -0700

    Adding without git files

---
+++ server/stomp-protocol-example-server/src/main/groovy/ua/naiksoftware/example_server/model/EchoModel.groovy
@@ -0,0 +1,13 @@
+package ua.naiksoftware.example_server.model
+
+/**
+ * Created by Naik on 23.02.17.
+ */
+class EchoModel {
+
+    EchoModel(String echo) {
+        this.echo = echo
+    }
+    String echo;
+    
+}
